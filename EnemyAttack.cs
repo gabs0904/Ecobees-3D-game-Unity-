bc689b9c5bb957e7dcfbc553661755d3840ee73a
Gabriela <71600185+gabs0904@users.noreply.github.com>
2025-05-19 10:20:56 +0200

    Update EnemyAttack.cs

--- EnemyAttack.cs
+++ EnemyAttack.cs
@@ -9,20 +9,13 @@ public class EnemyAttack : MonoBehaviour
     public float turnSpeed = 6f;
     public int damageDone = 1;
 
-    /// <summary>
-    /// Seconds waited after looking for player before looking again.
-    /// </summary>
     public float linecastInterval = 0.5f;
     public float linecastIntervalWhenChasing = 0.2f;
 
-    /// <summary>
-    /// Seconds waited after attacking before can attack again.
-    /// </summary>
+
     public float attackCooldown = 1.0f;
 
-    /// <summary>
-    /// How much enemy is knocked back when hit by a projectile.
-    /// </summary>
+
     public float shotKnockback = 10;
 
     public AudioClip hitPlayerSound;
@@ -100,9 +93,7 @@ public class EnemyAttack : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Reduce health when collided with a shot.
-    /// </summary>
+
     void OnTriggerEnter2D(Collider2D other)
     {
         ShotScript shot = other.gameObject.GetComponent<ShotScript>();
@@ -127,9 +118,7 @@ public class EnemyAttack : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Handle collision with walls.
-    /// </summary>
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (!hitWall && coll.gameObject.tag.ToLower() == "wall")
@@ -146,9 +135,7 @@ public class EnemyAttack : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Attack player, disable attacking for the duration of cooldown.
-    /// </summary>
+
     IEnumerator AttackPlayer(HealthScript health)
     {
         AudioSource.PlayClipAtPoint(hitPlayerSound, transform.position);
@@ -158,9 +145,7 @@ public class EnemyAttack : MonoBehaviour
         canAttack = true;
     }
 
-    /// <summary>
-    /// Look for the player, go after him if found.
-    /// </summary
+
     IEnumerator SeekPlayer()
     {
         canSeePlayer = !Physics2D.Linecast(transform.position, player.position, wallLayerMask);
@@ -188,10 +173,7 @@ public class EnemyAttack : MonoBehaviour
         isIdle = false;
         StopCoroutine("IdleBehaviour");
     }
-    /// <summary>
-    /// Move in short random bursts.
-    /// </summary>
-    /// <returns></returns>
+
     IEnumerator IdleBehaviour()
     {
         var direction = new Vector3(moveScript.direction.x, moveScript.direction.y, 0);
